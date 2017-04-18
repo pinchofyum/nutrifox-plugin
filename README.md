fd4455708623921cb2ae2b7210810bc265feaa3d
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-18 13:15:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nutrifox #
+**Contributors:** pinchofyum, danielbachhuber  
+**Tags:** blogging, recipes  
+**Requires at least:** 4.4  
+**Tested up to:** 4.7.3  
+**Stable tag:** 0.0.0  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
+
+Embed Nutrifox recipes in WordPress.
---
+++ nutrifox.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Nutrifox
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Embed Nutrifox recipes in WordPress.
+ * Author:          Pinch of Yum
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     nutrifox
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Nutrifox
+ */
